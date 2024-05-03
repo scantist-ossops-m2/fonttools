b8b19c0bcd5060548ec1eea756eae512e9495308
Behdad Esfahbod <behdad@behdad.org>
2024-05-03 08:07:54 -0600

    Black

--- Lib/fontTools/varLib/instancer/__init__.py
+++ Lib/fontTools/varLib/instancer/__init__.py
@@ -434,7 +434,6 @@ class AxisLimits(_BaseAxisLimits):
                 normalize(v, triple) for v in (minV, defaultV, maxV)
             )
 
-
         fvarAxes = fvar.getAxes()
         for tag, triple in normalizedLimits.items():
             minV, defaultV, maxV = fvarAxes[tag]
@@ -444,7 +443,9 @@ class AxisLimits(_BaseAxisLimits):
             distancePositive = maxV - defaultV
 
             if triple is None:  # Drop
-                normalizedLimits[tag] = NormalizedAxisTripleAndDistances(0, 0, 0, distanceNegative, distancePositive)
+                normalizedLimits[tag] = NormalizedAxisTripleAndDistances(
+                    0, 0, 0, distanceNegative, distancePositive
+                )
                 continue
 
             normalizedLimits[tag] = NormalizedAxisTripleAndDistances(
